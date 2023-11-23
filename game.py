b86267ed3b3afa53cea9811b6c1f8437f3eab90b
Hsin Pang <hpang19@my.bcit.ca>
2023-11-22 16:00:27 -0800

    Create make_character function

--- game.py
+++ game.py
@@ -8,7 +8,22 @@ def make_board(rows, columns):
 
 
 def make_character():
-    pass
+    """
+    Create a character.
+
+    The character's caffeine starts at 50 at level 1, and it has empty shopping bag and no tea consumed at the
+    beginning of the game, the final boss hasn't been killed.
+
+    :postcondition: return a dictionary as an initial character
+    :return: a dictionary representing the character
+    """
+    character = {
+        'caffeine': 50,
+        'shopping_bag': [],
+        'tea': [],
+        'kill_final_boss': False
+    }
+    return character
 
 
 def validate_move(level, board, character, direction):
