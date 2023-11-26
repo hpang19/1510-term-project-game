d0554eb9f9b3b3cf87b1cc8d22c9f39348586c2f
Hsin Pang <hpang19@my.bcit.ca>
2023-11-26 12:06:06 -0800

    add grocery store map into the borad

--- board/game_board.py
+++ board/game_board.py
@@ -2,8 +2,8 @@
 This module includes all features related to the game board.
 """
 
-from random import randint
-from rooms import kitchen_map, grocery_store, traditional_market
+from random import randint, choice
+from rooms import kitchen_map, grocery_store_map, traditional_market
 
 
 def make_board(rows, columns):
@@ -25,15 +25,21 @@ def make_board(rows, columns):
     kitchen_rows = int(rows / 2)
     kitchen_columns = int(columns / 2)
     chocolate_room = (randint(0, kitchen_rows - 1), randint(0, kitchen_columns - 1))
+    kitchen = kitchen_map((0, 0), kitchen_rows, kitchen_columns, chocolate_room)
+    grocery_store_origin = (choice(range(0, kitchen_rows - 2)), choice(range(kitchen_columns, columns - 2)))
+    grocery_store = grocery_store_map(grocery_store_origin, 3, 3)
+    grocery_row_range = range(grocery_store_origin[0], grocery_store_origin[0] + 3)
+    grocery_column_range = range(grocery_store_origin[1], grocery_store_origin[1] + 3)
     for row in range(rows):
         for column in range(columns):
             if row < kitchen_rows:
                 if column < kitchen_columns:
-                    kitchen = kitchen_map((0, 0), kitchen_rows, kitchen_columns, chocolate_room)
                     room = kitchen[(row, column)] if (row, column) in kitchen else 'Empty Room'
                     board[(row, column)] = ['L1', 'Kitchen', room]
                 else:
-                    board[(row, column)] = ['L2', 'Street', 'Empty Room']
+                    room = grocery_store[(row, column)] if (row, column) in grocery_store else 'Empty Room'
+                    location = 'Grocery Store' if (row in grocery_row_range and column in grocery_column_range) else 'Street'
+                    board[(row, column)] = ['L2', location, room]
             else:
                 board[(row, column)] = ['L3', 'Street', 'Empty Room']
     board[(rows - 1, columns - 1)] = ['L4', 'Destination', 'Joey and Hsin']
--- board/rooms.py
+++ board/rooms.py
@@ -5,25 +5,31 @@ This module includes all rooms on the game board.
 from random import choice
 
 
-def random_choice_coordinate(row_range, column_range):
+def random_choice_coordinate(origin_coordinate, rows, columns):
+    row_range = list(range(origin_coordinate[0], origin_coordinate[0] + rows))
+    column_range = list(range(origin_coordinate[1], origin_coordinate[1] + columns))
     return choice(row_range), choice(column_range)
 
 
 def kitchen_map(origin_coordinate, rows, columns, chocolate_room):
-    row_range = list(range(origin_coordinate[0], origin_coordinate[0]+rows))
-    column_range = list(range(origin_coordinate[1], origin_coordinate[1] + columns))
     kitchen = {(0, 0): 'Origin', chocolate_room: 'Chocolate Room'}
     for item in ['Cup', 'Spoon', 'Grinder', 'Hot Water', 'Tea Bag']:
-        row, column = random_choice_coordinate(row_range, column_range)
+        row, column = random_choice_coordinate(origin_coordinate, rows, columns)
         while (row, column) in kitchen:
-            row, column = random_choice_coordinate(row_range, column_range)
+            row, column = random_choice_coordinate(origin_coordinate, rows, columns)
         kitchen[row, column] = item
     return kitchen
 
 
-def grocery_store():
-    pass
+def grocery_store_map(origin_coordinate, rows, columns):
+    grocery_store = {(origin_coordinate[0] + choice(range(rows)), origin_coordinate[1]): 'Door'}
+    for item in ['Match Powder', 'Honey', 'Almond Milk']:
+        row, column = random_choice_coordinate(origin_coordinate, rows, columns)
+        while (row, column) in grocery_store:
+            row, column = random_choice_coordinate(origin_coordinate, rows, columns)
+        grocery_store[row, column] = item
 
+    return grocery_store
 
 def traditional_market():
     pass
