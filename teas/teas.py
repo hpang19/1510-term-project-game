7739ab57183e2f35f671f99af50c27131c5cd2cf
JoeyChocolate <echo35@my.bcit.ca>
2023-11-27 20:45:14 +0530

    Implement a function ready_to_make_tea when level one

--- teas/teas.py
+++ teas/teas.py
@@ -4,7 +4,10 @@ This module includes all features related to the teas.
 
 
 def ready_to_make_tea(level, character):
-    pass
+    if level == 1:
+        if "Hot Water" and "Mug" and "Tea Bag" in character["shopping_bag"]:
+            return True
+
 
 
 def make_tea(level, character):
