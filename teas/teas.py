c6be0046c72afec2d99f4475235cf0b0d4b2d2bc
Hsin Pang <hpang19@my.bcit.ca>
2023-11-27 21:18:46 -0800

    fix ready_to_make_tea

--- teas/teas.py
+++ teas/teas.py
@@ -6,25 +6,20 @@ This module includes all features related to the teas.
 def ready_to_make_tea(level, character):
     """
     >>> level_doctest01 = 1
-    >>> character_doctest01 = {
-    ...    'caffeine': 50,
-    ...    'coordinate': (0, 0),
-    ...    'shopping_bag': [],
-    ...    'tea': [],
-    ...    'kill_final_boss': False
-    ...}
+    >>> character_doctest01 = {'caffeine': 50, 'coordinate': (0, 0), 'shopping_bag': [], 'tea': [], 'kill_final_boss': False}
     >>> ready_to_make_tea(level_doctest01, character_doctest01)
     False
 
     """
-    if level == 1 and all("Hot Water" and "Mug" and "Tea Bag") in character["shopping_bag"]:
-        return True
-    if level == 2 and all(item in character["shopping_bag"] for item in
-                          ['Hot Water', 'Mug', 'Matcha Powder', 'Honey', 'Almond Milk', 'Spoon']):
-        return True
-    if level == 3 and all(item in character["shopping_bag"] for item in
-                          ['Hot Water', 'Mug', 'Ginger', 'Turmeric', 'Grater', 'Spoon']):
-        return True
+    if level == 1:
+        tea_bag_materials = {"Hot Water", "Mug", "Tea Bag"}
+        return tea_bag_materials.issubset(character["shopping_bag"])
+    if level == 2:
+        matcha_materials = {'Hot Water', 'Mug', 'Matcha Powder', 'Honey', 'Almond Milk', 'Spoon'}
+        return matcha_materials.issubset(character["shopping_bag"])
+    if level == 3:
+        ginger_tea_materials = {'Hot Water', 'Mug', 'Ginger', 'Turmeric', 'Grater', 'Spoon'}
+        return ginger_tea_materials.issubset(character["shopping_bag"])
 
 
 def make_tea(level, character):
