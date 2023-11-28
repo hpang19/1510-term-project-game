2df5ba614ba6a9531842ab9e27373356e6cdcb28
JoeyChocolate <echo35@my.bcit.ca>
2023-11-28 09:27:24 +0530

    Write a doctest for the function ready_to_make_tea

--- teas/teas.py
+++ teas/teas.py
@@ -4,6 +4,19 @@ This module includes all features related to the teas.
 
 
 def ready_to_make_tea(level, character):
+    """
+    >>> level_doctest01 = 1
+    >>> character_doctest01 = {
+    ...    'caffeine': 50,
+    ...    'coordinate': (0, 0),
+    ...    'shopping_bag': [],
+    ...    'tea': [],
+    ...    'kill_final_boss': False
+    ...}
+    >>> ready_to_make_tea(level_doctest01, character_doctest01)
+    False
+
+    """
     if level == 1:
         if "Hot Water" and "Mug" and "Tea Bag" in character["shopping_bag"]:
             return True
