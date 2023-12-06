c6f1156eec20df747a93d1f820d5984dd47a519c
Hsin Pang <hpang19@my.bcit.ca>
2023-12-06 00:29:11 -0800

    add doctest, docstring, and function annotation

--- challenges/foes.py
+++ challenges/foes.py
@@ -10,14 +10,38 @@ sys.path.append('..')
 from GUI import prompts
 
 
-def get_foe(current_room_description):
+def get_foe(current_room_description: list) -> str:
+    """
+    Get the foe based on the current room description.
+
+    :param current_room_description: a list
+    :precondition: current_room_description is a list consist of three elements [level, room, item]
+    :postcondition: get the foe with corresponding level and location
+    :return: a string representing the foe
+
+    >>> current_room_description = [1, 'Kitchen', "Nothing"]
+    >>> get_foe(current_room_description)
+    'rats'
+
+    >>> current_room_description = [2, 'Grocery Store', "Nothing"]
+    >>> get_foe(current_room_description)
+    'dogs'
+    """
     foe = FOE_MAP[current_room_description[1]]
     level = current_room_description[0]
     foe = foe[level-1] if current_room_description[1] == 'Street' else foe
     return foe
 
 
-def check_for_foes(current_room_description):
+def check_for_foes(current_room_description: list) -> bool:
+    """
+    Check for the presence of foes in the current room by matching two random choices.
+
+    :param current_room_description: a list
+    :precondition: current_room_description is a list consist of three elements [level, room, item]
+    :postcondition: check if foe exist by matching two random choices and return the result in boolean
+    :return: a boolean indicating if there are foes present
+    """
     foe = get_foe(current_room_description)
     if foe == 'rats':
         return choice(range(4)) == choice(range(4))
@@ -29,7 +53,18 @@ def check_for_foes(current_room_description):
         return True
 
 
-def rats_challenge(location, frame=None, text_area_object=None):
+def rats_challenge(location: str, frame: object = None, text_area_object: object = None) -> bool:
+    """
+    Engage in a challenge against rats in a specific location.
+
+    This function will print the message to ask player to kill the rats. When expected input is received,
+    it will return True.
+
+    :param location: a string indicating the location of the challenge
+    :precondition: location has to be a string and exist as one of the keys in LOCATION_PREFIX
+    :postcondition: return True after expected input is received, otherwise keep looping in the while loop
+    :return: True
+    """
     prompts.print_message(f'There is a rat {LOCATION_PREFIX[location]} {location.lower()}.\n', text_area_object)
     message = 'To proceed, you need to kill the rats. Please select a weapon to kill the rats from the list:\n'
     prompts.print_message(message, text_area_object)
@@ -54,7 +89,18 @@ def rats_challenge(location, frame=None, text_area_object=None):
                 prompts.print_message('You have to input an integer from the list:\n', text_area_object)
 
 
-def dogs_challenge(location, frame=None, text_area_object=None):
+def dogs_challenge(location: str, frame: object = None, text_area_object: object = None) -> bool:
+    """
+    Engage in a challenge against dogs in a specific location.
+
+    This function will print the message to ask player to choose a direction to dodge the dog attack. Function will
+    return True if the dodge direction is opposite of dog attack direction, and False otherwise.
+
+    :param location: a string indicating the location of the challenge
+    :precondition: location has to be a string and exist as one of the keys in LOCATION_PREFIX
+    :postcondition: return True if the dodge direction is opposite of dog attack direction, and False otherwise
+    :return: a boolean
+    """
     message = f'There is a dog {LOCATION_PREFIX[location]} {location.lower()}.\n'
     prompts.print_message(message, text_area_object)
     msg = 'Now the dog is trying to attack, you need to decide whether to dodge left or right. Please make a choice:\n'
@@ -78,7 +124,18 @@ def dogs_challenge(location, frame=None, text_area_object=None):
                 prompts.print_message('You have to input an integer from the list:\n', text_area_object)
 
 
-def kids_challenge(location, frame=None, text_area_object=None):
+def kids_challenge(location: str, frame: object = None, text_area_object: object = None) -> bool:
+    """
+    Engage in a challenge against kids in a specific location.
+
+    This function will print the message to ask player to answer a math question. Function will return True if the
+    answer is correct, and False otherwise.
+
+    :param location: a string indicating the location of the challenge
+    :precondition: location has to be a string and exist as one of the keys in LOCATION_PREFIX
+    :postcondition: return True if the answer is correct, and False otherwise
+    :return: a boolean
+    """
     message = f'There are kids running {LOCATION_PREFIX[location]} {location.lower()}.\n'
     prompts.print_message(message, text_area_object)
     message = 'You called school and the teacher comes. The teacher is challenging you with a Math question:\n'
@@ -97,7 +154,15 @@ def kids_challenge(location, frame=None, text_area_object=None):
     return your_answer == challenge_answer
 
 
-def boss_challenge(location, character, frame=None, text_area_object=None):
+def boss_challenge(location: str, character: dict, frame: object = None, text_area_object: object = None) -> None:
+    """
+    Engage in a challenge against the boss character at a specific location.
+
+    :param location: A string indicating the location of the challenge
+    :param character: A dictionary representing the game character
+    :precondition: location has to be a string and exist as one of the keys in LOCATION_PREFIX
+    :postcondition: caffeine level in the character will be dropped everytime the player answer a question wrong
+    """
     students = ['Joey', 'Hsin']
     message = f'There are {students[0]} and {students[1]} {LOCATION_PREFIX[location]} {location.lower()}.\n'
     prompts.print_message(message, text_area_object)
@@ -123,7 +188,15 @@ def boss_challenge(location, character, frame=None, text_area_object=None):
             prompts.print_message(f'Now {choice(students)} has another question:\n', text_area_object)
 
 
-def fight_with_foe(current_room, character, frame=None, text_area_object=None):
+def fight_with_foe(current_room: list, character: dict, frame: object = None, text_area_object: object = None):
+    """
+    Fight with a foe and calculate caffeine level based on fight result.
+
+    :param current_room: a list representing the current room's description
+    :param character: a dictionary representing the game character
+    :precondition: current_room is a list consist of three elements [level, room, item]
+    :precondition: character is a dictionary where "caffeine" exists as a key
+    """
     foe = get_foe(current_room)
     if foe == 'rats':
         rats_challenge(current_room[1], frame, text_area_object)
