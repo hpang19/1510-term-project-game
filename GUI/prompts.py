51a0cc7aa3d67c60b99b8c9d125e2f83aeb094ab
Hsin Pang <hpang19@my.bcit.ca>
2023-12-06 01:06:04 -0800

    add docstrinf

--- GUI/prompts.py
+++ GUI/prompts.py
@@ -3,7 +3,13 @@ import tkinter as tk
 
 
 class Prompts:
+    """
+    A class to create prompts for user input within a frame.
+    """
     def __init__(self, frame):
+        """
+        Initialize the Prompts class.
+        """
         self.frame = frame
         self.entry_value = tk.StringVar()
         self.greeting = None
@@ -11,16 +17,28 @@ class Prompts:
         self.entry_changed = False
 
     def on_return(self, event):
+        """
+        Handles the event when the "Return" key is pressed.
+        """
         self.entry_changed = True
 
     def wait_for_change(self):
+        """
+        Waits for changes in the entry value.
+        """
         if not self.entry_changed:
             self.frame.after(100, self.wait_for_change)
         else:
             self.greeting.pack_forget()
             self.entry.pack_forget()
 
-    def prompt(self, message):
+    def prompt(self, message: str) -> str:
+        """
+        Prompt for input and get the user input value.
+
+        :param message: a string
+        :return: a string input from player
+        """
         self.greeting = tk.Label(self.frame, text=message, width=50)
         self.greeting.pack()
         self.entry = tk.Entry(self.frame, textvariable=self.entry_value, width=5)
@@ -31,7 +49,13 @@ class Prompts:
         return self.entry_value.get()
 
 
-def print_message(message, text_area_object=None, clear=False):
+def print_message(message: str, text_area_object=None, clear=False):
+    """
+    Print a message to a text area object or the console.
+
+    :param message: a string of message to print
+    :postcondition: print a message to a text area object or the console
+    """
     if text_area_object:
         if clear:
             text_area_object.delete('1.0', tk.END)
@@ -40,7 +64,13 @@ def print_message(message, text_area_object=None, clear=False):
         print(message, end='')
 
 
-def add_lines(message, text_area_object):
+def add_lines(message: str, text_area_object):
+    """
+    Add lines to a text area object, maintaining a maximum of 15 lines.
+
+    :param message: a string of message to print
+    :postcondition: delete older text if more than 15 lines
+    """
     text_area_object.insert(tk.END, message)
     lines = text_area_object.get("1.0", tk.END).splitlines()
     if len(lines) > 15:
