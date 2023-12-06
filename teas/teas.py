b0934e7c956b36edc2f8a21aef0fab457e1baebe
Hsin Pang <hpang19@my.bcit.ca>
2023-12-06 01:17:49 -0800

    add docstring, doctest, and function annotation

--- teas/teas.py
+++ teas/teas.py
@@ -6,13 +6,24 @@ from . import TEA_MAP
 from GUI import prompts
 
 
-def ready_to_make_tea(level, character):
+def ready_to_make_tea(level: int, character: dict) -> bool:
     """
-    >>> level_doctest01 = 1
-    >>> character_doctest01 = {'caffeine': 50, 'coordinate': (0, 0), 'shopping_bag': [], 'tea': [], 'kill_final_boss': False}
-    >>> ready_to_make_tea(level_doctest01, character_doctest01)
+    Checks if the character is ready to make the tea at the given level.
+
+    :param level: an integer representing the current level
+    :param character: a dictionary representing the game character
+    :precondition: character contains "shopping_bag" in the key
+    :postcondition: return True if the character has all required materials, False otherwise
+    :return: a boolean
+
+    >>> character_test = {'caffeine': 50, 'coordinate': (0, 0), 'shopping_bag': [], 'tea': [], 'kill_final_boss': False}
+    >>> ready_to_make_tea(1, character_test)
     False
 
+    >>> character_test = {'caffeine': 50, 'coordinate': (0, 0), 'shopping_bag': ["Hot Water", "Mug", "Tea Bag"],
+    ... 'tea': [], 'kill_final_boss': False}
+    >>> ready_to_make_tea(1, character_test)
+    True
     """
     if level == 1:
         tea_bag_materials = {"Hot Water", "Mug", "Tea Bag"}
@@ -25,7 +36,19 @@ def ready_to_make_tea(level, character):
         return ginger_tea_materials.issubset(character["shopping_bag"])
 
 
-def make_tea(level, character, text_area_object=None):
+def make_tea(level: int, character: dict, text_area_object=None):
+    """
+    Print a message saying player has made tea for the character at the given level and increases their caffeine.
+
+    :param level: an integer representing the current level
+    :param character: a dictionary representing the game character
+    :precondition: character contains "tea" and "caffeine" in the key
+    :postcondition: increase character's caffeine level
+
+    >>> character_test = {'caffeine': 50, 'coordinate': (0, 0), 'shopping_bag': [], 'tea': [], 'kill_final_boss': False}
+    >>> make_tea(1, character_test)
+    You made Green Tea, drink it, and your caffeine increased to 100
+    """
     character['tea'].append(TEA_MAP[level])
     character['caffeine'] += 50 * level
     message = f'You made {TEA_MAP[level]}, drink it, and your caffeine increased to {character["caffeine"]}\n'
