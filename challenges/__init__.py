1094e2528cbcb557948043c264f11c1378cf4fbb
Hsin Pang <hpang19@my.bcit.ca>
2023-12-08 10:40:01 -0800

    add more questions to question bank

--- challenges/__init__.py
+++ challenges/__init__.py
@@ -18,10 +18,37 @@ RATS_WEAPONS = {
 
 MATH_QUESTIONS = {
     '5x + 5 = 10; Therefore x = ': '1',
-    'What is the square root of 10000?': '100',
+    'What is the square root of 10000? ': '100',
+    'What is 7 multiplied by 8? ': '56',
+    'If you add 15 to 30, what do you get? ': '45',
+    'Calculate: 25 divided by 5 = ': '5',
+    'What is the value of 3 squared? ': '9',
+    'Subtract 12 from 20 = ': '8',
+    'If a dozen is 12, how many is half a dozen? ': '6',
+    'What is 4 times 9? ': '36',
+    'Add 17 to 23 = ': '40',
+    'What is 64 divided by 8? ': '8',
+    'Calculate: 15 + 25 - 10 = ': '30',
+    'If you have 3 apples and give 1 to a friend, how many do you have left? ': '2',
+    'What is the next number in the sequence: 2, 4, 6, 8, ___? ': '10',
+    'If a rectangle has a length of 5 units and a width of 3 units, what is its area? ': '15',
+    'What is 3 squared minus 7? ': '2',
+    'Add 50 and 25 = ': '75',
+    'Subtract 15 from 40 = ': '25',
+    'If you have 10 candies and give away half, how many candies do you have left? ': '5',
+    'What is 9 multiplied by 6? ': '54',
+    'Calculate: 72 divided by 9 = ': '8'
 }
 
 PYTHON_QUESTIONS = {
     'List is mutable. (Enter "T" for True or "F" for False)': 'T',
     'Python is static typed language. (Enter "T" for True or "F" for False)': 'F',
+    'Is Python case-sensitive? (Enter "T" for True or "F" for False)': 'T',
+    'What symbol is used for comments in Python? (Choose "#" or "//")': '#',
+    'Is Python an interpreted language? (Enter "T" for True or "F" for False)': 'T',
+    'Which keyword is used to define a function in Python? (Choose "def" or "function")': 'def',
+    'Does Python use indentation to define blocks of code? (Enter "T" for True or "F" for False)': 'T',
+    'What is the result of 5 % 2 in Python?': '1',
+    'What data type is used to store a sequence of characters in Python? (Choose "str" or "int")': 'str',
+    'Is "None" a keyword in Python? (Enter "T" for True or "F" for False)': 'T'
 }
